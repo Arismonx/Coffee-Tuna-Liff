a556cbb17d095477cc874fa8dbf9d8f78103196f
Arismonx <arismonx@gmail.com>
2026-04-23 02:06:44 +0700

    Add LineHandler and webhook functionality to handle Events Webhook from LINE server

---
+++ backend/handler/line.go
@@ -0,0 +1,149 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Arismonx/Coffee-Tuna-Liff/config"
+	"github.com/gin-gonic/gin"
+)
+
+// this past is format for Events Webhook from request line server
+// more detail https://developers.line.biz/en/reference/messaging-api/#webhooks
+
+// strcut events
+type EventsPayload struct {
+	Events []WebhookEvent
+}
+
+type WebhookEvent struct {
+	Type       string        `json:"type"`
+	ReplyToken string        `json:"replyToken"`
+	Message    EventMessage  `json:"message"`
+	Postback   EventPostback `json:"postback"`
+}
+
+type EventPostback struct {
+	Data string `json:"data"`
+}
+
+type EventMessage struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+// struct JSON Reply Payload send to line server
+type Message struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+type ReplyPayload struct {
+	ReplyToken string    `json:"replyToken"`
+	Messages   []Message `json:"messages"`
+}
+
+func CreateSendRequesReply(payload ReplyPayload, Token string, url string) {
+	// convert struct to string JSON (look like JSON.stringify)
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Create Requset to Line Server
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set Header
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+Token)
+
+	// Sent Request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+
+	// Close
+	defer req.Body.Close()
+
+	// Check LINE Response
+	fmt.Println("LINE Response Status:", res.Status)
+}
+
+// strcut config
+type LineHandler struct {
+	Config config.Config
+}
+
+// Construct LineHandler
+func NewLineHandler(Config config.Config) *LineHandler {
+	return &LineHandler{
+		Config: Config,
+	}
+}
+
+// Bind this func with struct LineHandler and can use attribute in struct
+func (this *LineHandler) Webhook(ctx *gin.Context) {
+	token := this.Config.LineChannelAccessToken
+
+	// URL Reply
+	url := "https://api.line.me/v2/bot/message/reply"
+
+	// object event data webhook from request line server
+	var body EventsPayload
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		fmt.Println("Error Bind JSON:", err)
+		ctx.JSON(400, gin.H{"error": err})
+		return
+	}
+
+	// Check events data have object events
+	if len(body.Events) > 0 {
+		event_0 := body.Events[0]
+
+		user_message := event_0.Message.Text
+		fmt.Println("User Message: ", user_message)
+
+		// Check type is type message
+		if event_0.Type == "message" {
+
+			// Payload Data Reply
+			payload := ReplyPayload{
+				ReplyToken: event_0.ReplyToken,
+				Messages: []Message{
+					{Type: "text", Text: user_message},
+				},
+			}
+
+			CreateSendRequesReply(payload, token, url)
+
+		}
+
+		// Check type is type postback
+		if event_0.Type == "postback" {
+			fmt.Println("Data: ", event_0.Postback.Data)
+			if event_0.Postback.Data == "ปุ่มAนะ" {
+				payload := ReplyPayload{
+					ReplyToken: event_0.ReplyToken,
+					Messages: []Message{
+						{Type: "text", Text: "ปุ่มAนะ"},
+					},
+				}
+
+				CreateSendRequesReply(payload, token, url)
+			}
+		}
+
+	}
+	ctx.JSON(200, gin.H{"message": "HTTP POST request sent to the webhook URL!"})
+}
